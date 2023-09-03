8c0d8672728b96d7c07db112ab2b4335611d30c6
Maxim <uservice825@gmail.com>
2023-09-04 00:27:41 +0300

    Resolution changed from 800*600 to 1200*800

--- src/strelnikov/Main.java
+++ src/strelnikov/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new MainFrame();
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(800, 600);
+        frame.setSize(1200, 800);
         frame.setVisible(true);
     }
 }
